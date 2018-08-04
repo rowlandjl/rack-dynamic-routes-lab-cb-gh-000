bc5ad6311257f89c12547a35a5ed150cbd35f328
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-04 01:56:24 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application 
+  def call(env)
+    response = Rack::Response.new 
+    request = Rack::Response.new(env)
+
+    if request.path.match(/items/)
+      item_name = request.path.split('/items/').last 
+      item = Item.all.detect { |item| item.name == item_name}
+      if item 
+        response.write item.price 
+      else 
+        response.write 'Item not found'
+        response.status = 400 
+      end 
+    else 
+      response.write 'Route not found'
+      response.status = 404
+    end 
+
+    response.finish 
+  end 
+end 
