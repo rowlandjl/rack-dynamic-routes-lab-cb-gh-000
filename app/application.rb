2cbc1fa22492fd3afff7180677bf16bc0726fd6b
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-04 01:56:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,22 @@
-class Application 
+class Application
   def call(env)
-    response = Rack::Response.new 
+    response = Rack::Response.new
     request = Rack::Response.new(env)
 
     if request.path.match(/items/)
-      item_name = request.path.split('/items/').last 
+      item_name = request.path.split('/items/').last
       item = Item.all.detect { |item| item.name == item_name}
-      if item 
-        response.write item.price 
-      else 
+      if item
+        response.write item.price
+      else
         response.write 'Item not found'
-        response.status = 400 
-      end 
-    else 
+        response.status = 400
+      end
+    else
       response.write 'Route not found'
       response.status = 404
-    end 
+    end
 
-    response.finish 
-  end 
+    response.finish
+  end
 end 
